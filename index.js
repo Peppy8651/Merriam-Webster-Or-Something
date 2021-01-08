125049230cd234fa37f6d4749286104cac05b1c5
Peppy8651 <73673342+Peppy8651@users.noreply.github.com>
2021-01-08 09:42:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ module.exports = class {
 		const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${Word}?key=${this.token}`;
 		// eslint-disable-next-line no-unused-vars
 		const fetchedResults = await nodeFetch(url).then(response => response.json()).catch(e => null);
-		if (fetchedResults == 'Key is required.' || fetchedResults == 'Invalid API key. Not subscribed for this reference.') throw new Error('Key is invalid or has not been added in the constructor. Add a valid key to the constructor.');
 		if (!fetchedResults) return null;
 		const res = new Results(fetchedResults);
 		return res;
